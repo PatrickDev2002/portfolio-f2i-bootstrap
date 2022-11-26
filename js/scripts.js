0f3f3b68b4663b7b56d1061349bd1aa5104e8e5f
PatrickDev2002 <patrick@fewlines.co>
2022-11-26 19:30:08 +0100

    main: portfolio fini

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Fonction navbar mini
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Minimiser la navbar
+    navbarShrink();
+
+    // Minimiser la navbar quand on scroll
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activer Bootstrap scrollspy sur l'élément de navigation principal
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 72,
+        });
+    };
+
+    // Minimiser la barre de navigation réactive lorsque la bascule est visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+});
